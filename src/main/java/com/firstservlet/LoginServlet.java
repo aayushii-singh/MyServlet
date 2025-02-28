c3d1225e7cfe058f065ce3a497b01d479e07cc07
aayushii-singh <122150257+aayushii-singh@users.noreply.github.com>
2025-02-28 22:11:44 +0530

    Added:Regex-Validate name

--- src/main/java/com/firstservlet/LoginServlet.java
+++ src/main/java/com/firstservlet/LoginServlet.java
@@ -24,14 +24,22 @@ public class LoginServlet extends HttpServlet{
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        String nameRegex="^[A-Z]{1}[a-z]{3,}$";
         String user = req.getParameter("user");
         String pwd = req.getParameter("pwd");
 
 
         String userID = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
-
+        if((userID.equals(user) && password.equals(pwd))&&Pattern.matches(nameRegex,userID)) {
+            req.setAttribute("user",user);
+            req.getRequestDispatcher("LoginSuccess.jsp").forward(req, resp);
+        } else {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out  = resp.getWriter();
+            out.println("<font color = red> Either username or password is wrong</font>");
+            rd.include(req, resp);
+        }
 
     }
 
